8015a23e2b9fd9ecc648d42ab8d151ce675c3701
mechaniccoder <yuseunghwan94@gmail.com>
2021-07-30 00:42:39 +0900

    Asset Management

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,37 +1,27 @@
-import webpack from 'webpack';
-import path from 'path';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin'
-import CssMinimizerPlugin from 'css-minimizer-webpack-plugin'
+import webpack from "webpack";
+import path from "path";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import CssMinimizerPlugin from "css-minimizer-webpack-plugin";
 
 const config: webpack.Configuration = {
   mode: "production",
   entry: "./src/index.js",
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js",
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
-      }
-    ]
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
   },
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new CssMinimizerPlugin({
-        parallel: true,
-      }),
-    ]
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css",
-    })
-  ]
-}
+};
 
 export default config;
