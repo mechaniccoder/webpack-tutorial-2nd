b8a4750fab4646adbea9b8388c09519300972b5d
mechaniccoder <yuseunghwan94@gmail.com>
2021-07-30 01:05:13 +0900

    Development

--- webpack.config.ts
+++ webpack.config.ts
@@ -2,22 +2,24 @@ import webpack from "webpack";
 import path from "path";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 
-const config: webpack.Configuration = {
-  mode: "production",
-  entry: {
-    index: "./src/index.js",
-    print: "./src/print.js",
-  },
-  output: {
-    path: path.join(__dirname, "dist"),
-    filename: "[name].bundle.js",
-    clean: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Output Management",
-    }),
-  ],
+const config = (env: any): webpack.Configuration => {
+  return {
+    mode: env.NODE_ENV,
+    entry: {
+      index: "./src/index.js",
+      print: "./src/print.js",
+    },
+    output: {
+      path: path.join(__dirname, "dist"),
+      filename: "[name].bundle.js",
+      clean: true,
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: "Development",
+      }),
+    ],
+  };
 };
 
 export default config;
