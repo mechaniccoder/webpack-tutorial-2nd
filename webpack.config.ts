862ad4f0ded96e1cb9822af41b33dfc4ea48d34d
mechaniccoder <yuseunghwan94@gmail.com>
2021-07-30 00:21:36 +0900

    Asset management

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,13 +1,37 @@
 import webpack from 'webpack';
 import path from 'path';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+import CssMinimizerPlugin from 'css-minimizer-webpack-plugin'
 
 const config: webpack.Configuration = {
   mode: "production",
   entry: "./src/index.js",
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'main.js'
-  }
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      }
+    ]
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin({
+        parallel: true,
+      }),
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+    })
+  ]
 }
 
 export default config;
