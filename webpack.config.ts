ad0a33f49acc51a8af40b9c39fbf32cd61fa0dcb
mechaniccoder <yuseunghwan94@gmail.com>
2021-08-02 23:26:01 +0900

    Authoring library

--- webpack.config.ts
+++ webpack.config.ts
@@ -5,30 +5,18 @@ import HtmlWebpackPlugin from "html-webpack-plugin";
 const config = (env: any): webpack.Configuration => {
   return {
     mode: env.NODE_ENV,
-    entry: {
-      index: "./src/index.js",
-    },
+    entry: "./src/library.js",
     output: {
-      path: path.join(__dirname, "dist"),
-      filename: "[name].[contenthash].bundle.js",
-      clean: true,
+      path: path.join(__dirname, "libary"),
+      filename: "index.js",
+      library: "MyLibrary",
     },
-    plugins: [
-      new HtmlWebpackPlugin({
-        title: "Code Splitting",
-      }),
-    ],
-    optimization: {
-      moduleIds: "deterministic",
-      runtimeChunk: "single",
-      splitChunks: {
-        cacheGroups: {
-          vendor: {
-            test: /[\\/]node_modules[\\/]/,
-            name: "node_modules",
-            chunks: "all",
-          },
-        },
+    externals: {
+      lodash: {
+        commonjs: "lodash",
+        commonjs2: "lodash",
+        amd: "lodash",
+        root: "_",
       },
     },
   };
